b6d99e1d268c67ac70292c4245b530b1d67aa8c9
djaden927 <djaden92@gmail.com>
2021-07-14 17:54:59 -0600

    finished assignment

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,31 @@
 // STRETCH
+const cars = [
+    {
+    vin: '1111111111111',
+    make: 'toyota',
+    model: 'prius',
+    mileage: 215000,
+    title: 'clean',
+    transmission: 'manual',
+},
+{
+    vin: '1111111111113',
+    make: 'corolla',
+    model: 'prius',
+    mileage: 2100,
+    title: 'salvage',
+},
+{
+    vin: '1111111121111',
+    make: 'ford',
+    model: 'focus',
+    mileage: 5000,
+    title: 'clean',
+    transmission: 'manual',
+}
+]
+
+exports.seed = async function (knex){
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
